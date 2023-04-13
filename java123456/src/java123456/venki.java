61763cea65c20fa0ffd0acd2f5cc69e9278234dd
venkey <venkey@gmail.com>
2023-04-13 09:52:59 +0530

    Push the commit Git rep

---
+++ java123456/src/java123456/venki.java
@@ -0,0 +1,21 @@
+package java123456;
+
+public class venki {
+
+	public static void main(String[] args) {
+	    
+	    {
+	        String input = "GeeksForGeeks";
+	        char[] try1 = input.toCharArray();
+	        for (int i = try1.length - 1; i >= 0; i--)
+	            System.out.print(try1[i]);
+	    }
+	
+
+	}
+
+
+
+	}
+
+
